e5daf136435becd8b57a1c71b834b8c83f492788
MichaelEnny <moe0100@mcdaniel.edu>
2026-05-01 08:56:08 -0700

    first commit

---
+++ project_1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="YT_SEO_INSIGHTS_GENERATOR",
+    version="0.0.1",
+    author="",
+    author_email="",
+    packages=find_packages(),
+    install_requires=[],
+)
